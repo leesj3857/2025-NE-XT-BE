7fc6eadb08a14fc9b10806d37c7198c38c6bd474
leesj3857 <hensin12@gmail.com>
2025-04-16 19:38:56 +0900

    urls.py에 import에 마이그레이션 추가

--- translation_api/urls.py
+++ translation_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import translate_category
+from .views import translate_category, run_migrate
 
 urlpatterns = [
     path('translate/', translate_category, name='translate_category'),  # ✅ 경로 일치
